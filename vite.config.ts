5856fd447c116fdc5554601a7946f2fcb06b7143
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-09-08 20:07:14 -0400

    Based on new Tauri/Vue template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  plugins: [vue()],
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
