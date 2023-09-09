7bfa1d47b851666160a53a1ed5fe9ac2ce0f6187
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-09-09 15:33:37 -0400

    Got MDI Icons working

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from './router';
 
 // Vuetify
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -12,6 +13,7 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
     components,
     directives,
+    
 })
 
 createApp(App).use(router).use(vuetify).mount('#app');
