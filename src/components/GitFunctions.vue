ab3767c4c2461bd8cd87cc28d2f76a00cc7802cf
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-09-09 15:08:20 -0400

    LeftNavBar basic implementation

--- src/components/GitFunctions.vue
+++ src/components/GitFunctions.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="git-functions">
-    <router-view></router-view>
+    <router-view>
+      <p>GitFunctions</p>
+    </router-view>
   </div>
 </template>
--- src/components/LeftNavBar.vue
+++ src/components/LeftNavBar.vue
@@ -1,46 +1,38 @@
 <template>
-  <v-navigation-drawer app color="primary" dark>
+  <v-navigation-drawer app>
     <v-list>
-      <v-list-item-group>
-        <v-list-item to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item to="/equipment">
-          <v-list-item-icon>
-            <v-icon>mdi-server</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Equipment</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item to="/diff">
-          <v-list-item-icon>
-            <v-icon>mdi-diff</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Diff</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <!-- Add more navigation items here -->
-      </v-list-item-group>
+      <v-list-item v-for="route in routes" :key="route.name" :to="route.path">
+        <v-list-item-icon>
+          <v-icon>{{ route.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ route.name }}</v-list-item-title>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'LeftNavBar',
-  // Add any component-specific logic here
+  setup() {
+    const routes = ref([
+      { path: '/', name: 'MainContent', icon: 'mdi-home' },
+      { path: '/equipment', name: 'EquipmentList', icon: 'mdi-server' },
+      { path: '/diff', name: 'DiffView', icon: 'mdi-diff' },
+      { path: '/git', name: 'GitFunctions', icon: 'mdi-git' },
+      { path: '/file', name: 'FileFunctions', icon: 'mdi-file' },
+      { path: '/settings', name: 'Settings', icon: 'mdi-cog' },
+    ]);
+
+    return {
+      routes,
+    };
+  },
 });
 </script>
 
 <style scoped>
-/* Add any additional styling here */
+/* Add your styles here */
 </style>
