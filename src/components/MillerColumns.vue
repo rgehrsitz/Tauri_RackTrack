37941250ce48b6f43a7249eec2ce88cc2c3f63c9
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-09-14 18:35:31 -0400

    Updated MillerColumn SFC

--- src/components/MillerColumns.vue
+++ src/components/MillerColumns.vue
@@ -1,65 +1,67 @@
 <template>
-    <div class="miller-columns">
-      <div v-for="(column, index) in columns" :key="index" class="column">
-        <div
-          v-for="item in column"
-          :key="item.uuid"
-          @click="selectItem(item, index)"
-          class="item"
-        >
-          {{ item.name }}
-        </div>
+  <div class="miller-columns">
+    <div v-if="rootEquipment" v-for="(column, index) in displayedColumns.slice(0, 3)" :key="index" class="column">
+      <div v-for="item in column" :key="item.uuid" @click="selectItem(item, index)" class="item" v-tooltip="item.name">
+        {{ item.name }}
       </div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from "vue";
-  import { Equipment } from "../backend/models/equipment";
-  
-  export default defineComponent({
-    props: {
-      rootEquipment: {
-        type: Object as () => Equipment,
-        required: true,
-      },
-    },
-    setup(props) {
-      const columns = ref<Equipment[][]>([[props.rootEquipment]]);
-  
-      const selectItem = (item: Equipment, columnIndex: number) => {
-        // Remove columns after the selected one
-        columns.value = columns.value.slice(0, columnIndex + 1);
-  
-        // Add a new column if the selected item has children
-        if (item.children.length > 0) {
-          columns.value.push(item.children);
-        }
-      };
-  
-      return {
-        columns,
-        selectItem,
-      };
-    },
-  });
-  </script>
-  
-  <style scoped>
-  .miller-columns {
-    display: flex;
-    overflow-x: auto;
+
+    <div v-if="displayedColumns.length > 6" class="column-hidden-indicator">
+      ...
+    </div>
+
+    <div v-if="rootEquipment" v-for="(column, index) in displayedColumns.slice(-3)" :key="index" class="column">
+      <div v-for="item in column" :key="item.uuid" class="item" v-tooltip="item.name">
+        {{ item.name }}
+      </div>
+    </div>
+
+    <div v-else class="empty-column">
+      No file open.
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue'
+import { Equipment } from "../backend/models/equipment"
+
+const rootEquipment = ref<Equipment | null>(null)
+const actualColumns = ref<Equipment[][]>([])
+const columns = computed(() => rootEquipment.value ? [[rootEquipment.value]] : [])
+
+watch(columns, (newVal) => {
+  actualColumns.value = newVal
+})
+
+const displayedColumns = computed(() => {
+  // Logic to determine which columns to display based on actualColumns.value
+  return actualColumns.value
+})
+
+const selectItem = (item: Equipment, columnIndex: number) => {
+  // Logic to handle item selection and column display
+  if (item.children && item.children.length > 0) {
+    actualColumns.value = actualColumns.value.slice(0, columnIndex + 1).concat([item.children])
+  } else {
+    actualColumns.value = actualColumns.value.slice(0, columnIndex + 1)
   }
-  .column {
-    min-width: 200px;
-    border-right: 1px solid #ccc;
-  }
-  .item {
-    padding: 10px;
-    cursor: pointer;
-    &:hover {
-      background-color: #f0f0f0;
-    }
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+/* ... (existing styles) */
+
+.item {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 200px;
+}
+
+.empty-column {
+  padding: 20px;
+  text-align: center;
+  color: #aaa;
+}
+</style>
