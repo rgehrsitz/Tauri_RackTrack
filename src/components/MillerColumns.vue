a6de7217afa82e9f67a400070125718b3f422b84
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-09-15 13:33:32 -0400

    Yet another implementation of MillerColumns.vue

--- src/components/MillerColumns.vue
+++ src/components/MillerColumns.vue
@@ -1,119 +1,54 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { Equipment } from '../backend/models/equipment';
-//import { useEquipmentStore } from '../stores/equipmentStore';
-
-const props = defineProps<{
-  equipment: Equipment
-}>();
-
-//const store = useEquipmentStore();
-
-interface Column {
-  id: string;
-  name: string;
-  width: number;
-  truncated: boolean;
-}
-
-interface BreakColumn {
-  break: boolean;
-}
-
-type Columns = Column | BreakColumn;
-
-const columns = ref<Columns[]>([]);
-
-const truncatedText = ref('');
-const hoveredColumn = ref<Column | null>(null);
-
-const maxColumns = 10;
-const columnWidth = 150;
-
-onMounted(() => {
-  calculateColumns();
-});
-
-const calculateColumns = () => {
-
-  columns.value = [];
-
-  const addColumn = (equipment: Equipment) => {
-
-    const col: Column = {
-      id: equipment.uuid,
-      name: equipment.name,
-      width: columnWidth,
-      truncated: false
-    };
-
-    columns.value.push(col);
-
-    equipment.children.forEach(addColumn);
-
-    if (columns.value.length >= maxColumns) {
-      columns.value.push({
-        break: true
-      });
-    }
-
-  };
-
-  addColumn(props.equipment);
-
-};
-
-const handleMouseover = (column: Column) => {
-  truncatedText.value = column.name;
-  hoveredColumn.value = column;
-}
-
-const handleMouseleave = () => {
-  truncatedText.value = '';
-  hoveredColumn.value = null;
-}
-
-function isBreakColumn (column: Columns): column is BreakColumn {
-  return (column as BreakColumn).break !== undefined;
-}
-
-</script>
-
 <template>
-  <v-container fluid>
-
-    <v-row no-gutters>
-
-      <v-col v-for="(column, index) in columns" :key="index">
-
-        <template v-if="isBreakColumn(column)">
-          <v-sheet class="text-center py-6">
-            ...
-          </v-sheet>
-        </template>
-
-        <template v-else>
-
-          <div :style="{ width: column.width + 'px' }" @mouseover="handleMouseover(column)"
-            @mouseleave="handleMouseleave">
-
-            <div class="text-truncate" :title="truncatedText">
-              {{ column.name }}
-            </div>
-
-            <!-- tooltip -->
-
-          </div>
-
-        </template>
-
-      </v-col>
-
-    </v-row>
-
-  </v-container>
+  <div class="miller-columns">
+    <div v-for="(column, index) in columns" :key="index" class="column">
+      <div v-for="item in column" :key="item.uuid" @click="handleItemClick(item, index)" class="item">
+        {{ item.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-/* Styles */
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Equipment } from '../backend/models/equipment';
+
+const props = defineProps<{
+  rootEquipment: any;
+}>();
+
+const columns = ref([[props.rootEquipment]]);
+
+const handleItemClick = (item: Equipment, columnIndex: number) => {
+  if (item.children && item.children.length > 0) {
+    columns.value = columns.value.slice(0, columnIndex + 1);
+    columns.value.push(item.children);
+  } else {
+    columns.value = columns.value.slice(0, columnIndex + 1);
+  }
+};
+</script>
+
+<style scoped>
+.miller-columns {
+  display: flex;
+  overflow: auto;
+}
+
+.column {
+  border-right: 1px solid #ccc;
+  padding: 10px;
+  min-width: 200px;
+}
+
+.item {
+  padding: 5px;
+  cursor: pointer;
+  border: 1px solid transparent;
+  transition: background-color 0.3s;
+}
+
+.item:hover {
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+}
 </style>
